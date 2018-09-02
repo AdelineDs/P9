b0ca7d2b31922f57c00037cc5ede09980154ec9e
Adeline <deca.adeline@gmail.com>
2018-09-02 08:26:01 +0200

    test documentation du code sur le controleur pour les likes

--- Controller/ControllerVote.php
+++ Controller/ControllerVote.php
@@ -10,15 +10,31 @@ namespace AdelineD\OC\P9\Controller;
 
 use \AdelineD\OC\P9\Model\Vote;
 
+/**
+ * Class ControllerVote
+ * @package AdelineD\OC\P9\Controller
+ */
 class ControllerVote extends ControllerMain
 {
+    /**
+     * @var Vote
+     */
     private $vote;
 
+    /**
+     * ControllerVote constructor.
+     */
     public function __construct()
     {
         $this->vote = new Vote();
     }
 
+    /**
+     * @param $photoId int id of the photo for which a member voted
+     * @param $memberId int id of the member who voted
+     * @param $likedMemberId int id of the member owning the photo
+     * @throws \Exception
+     */
     public function vote($photoId, $memberId, $likedMemberId){
         $this->vote->doVote($photoId, $memberId);
         header('Location: index.php?action=member&id='.$likedMemberId);
